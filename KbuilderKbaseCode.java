7f3a761bc4de23aedba9b8619f34e57c033ad0e9
Ajay Chowdary Kandula <mailajayck@gmail.com>
2021-06-09 08:53:24 -0400

    Add files via upload

---
+++ KbuilderKbaseCode.java
@@ -0,0 +1,19 @@
+public static final KieSession newStatefulKnowledgeSession(final Class<?> clazz, final String drl, final Map<String, String> properties) {
+final KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+kbuilder.add(ResourceFactory.newClassPathResource(drl, clazz), ResourceType.DRL);
+
+if (kbuilder.hasErrors()) {
+	throw new Error(kbuilder.getErrors().toString());
+	}
+	
+	final KnowledgeBase kbase = KnowledgeFactory.newKnowledgeBase();
+	kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+	
+	final KieSessionConfiguration config = KnowledgeBaseFactory.newKnowledgeSessionConfiguration();
+	for (final Map.Entry<String, String) property : properties.entrySet()) {
+		config.setProperty(property.getKey(), property.getValue());
+	}
+	
+	return kbase.newStatefulKnowledgeSession(config, null);
+
+}
